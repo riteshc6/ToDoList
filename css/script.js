a3586ae116fe197f2400fbe2878bca9497b1d100
Ritesh Chaurasia <riteshc6@gmail.com>
2019-06-04 20:05:42 +0530

    Added feature to strike through completed tasks' text on clicking checkbox

--- css/script.js
+++ css/script.js
@@ -6,9 +6,16 @@ text.addEventListener("keypress", (e) => {
 
         // Create elements to be added to html
         let next = e.target.nextElementSibling
-        console.log(next)
+        
         const li = document.createElement("li")
+        const input = document.createElement("input")
+        input.setAttribute('type', 'checkbox')
+        input.setAttribute('id', 'toggle')
+        li.appendChild(input)
+
         const label = document.createElement("label")
+
+
         label.textContent = e.target.value
         
         li.appendChild(label)
@@ -17,3 +24,27 @@ text.addEventListener("keypress", (e) => {
     }
 })
 
+// Complete tasks
+
+
+document.addEventListener("click", (e) => {
+    
+    if (e.target.id == "toggle"){
+        console.log(e.target.checked)
+        if (e.target.checked) {
+            let parent = e.target.parentNode
+            let label = parent.querySelector("label")
+            label.style.textDecoration = "line-through"
+            label.style.color = "gray"
+            console.log(label)
+        }
+        else {
+            let parent = e.target.parentNode
+            let label = parent.querySelector("label")
+            label.style.textDecoration = "none"
+            label.style.color = "black"
+            console.log(label)
+        }
+    }
+    
+})
