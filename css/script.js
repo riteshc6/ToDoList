74c51137fffd9383e32bd65982a37110f38daeea
Ritesh Chaurasia <riteshc6@gmail.com>
2019-06-06 18:30:19 +0530

    added dom refresh

--- css/script.js
+++ css/script.js
@@ -1,11 +1,12 @@
 // Collecting text Input
 let text = document.getElementById("task")
 let todo = ""
-let obj = {}
+let data = {}
 var count = 0
+
 text.addEventListener("keypress", (e) => {
     if (e.keyCode === 13) {
-
+        let obj = {}
         // Create elements to be added to html
         let next = e.target.nextElementSibling
 
@@ -27,7 +28,7 @@ text.addEventListener("keypress", (e) => {
 
 
         label.textContent = e.target.value
-
+       
         let div = document.createElement("div")
         div.appendChild(input)
         div.appendChild(label)
@@ -42,32 +43,44 @@ text.addEventListener("keypress", (e) => {
         li.style.alignItems = "center"
         li.style.justifyContent = "space-between"
         li.querySelector(".cross").style.visibility = "hidden"
+        obj["text"] = e.target.value
+        obj["completed"] = false
+        obj["li"] = li
+        data[count - 1] = obj
         next.appendChild(li)
-        
+        console.log(data)
         e.target.value = ""
     }
 })
 
-// Complete tasks
+//-------------------------------------------------------------Completed tasks-------------------------------------------------------
 
 document.addEventListener("click", (e) => {
 
     if (e.target.id == "toggle") {
-        console.log(e.target.checked)
+        
         if (e.target.checked) {
             let parent = e.target.parentNode
+            let id = parent.parentNode.getAttribute("id")
+            data[id].completed = true
             let label = parent.querySelector("label")
             label.style.textDecoration = "line-through"
             label.style.color = "gray"
 
         } else {
+            
             let parent = e.target.parentNode
             let label = parent.querySelector("label")
+            let id = parent.parentNode.getAttribute("id")
+            data[id].completed = false
+            
             label.style.textDecoration = "none"
             label.style.color = "black"
 
         }
+    domRefresh()
     }
+
 
 })
 
@@ -106,7 +119,7 @@ active.addEventListener("click", (active) => {
     });
 })
 
-// -----------------------COMPLETED---------------------------
+// -----------------------COMPLETED TAB---------------------------
 let completed = document.querySelector("#completed")
 completed.addEventListener("click", () => {
     
@@ -129,7 +142,7 @@ completed.addEventListener("click", () => {
 
 
 
-// Show delete task option upon hover and perform delete upon click on cross
+//----------------------------------------------------- Show Delete task option upon hover and perform delete upon click on cross
 
 let ul = document.querySelector("ul")
 
@@ -141,9 +154,14 @@ ul.addEventListener("mouseenter", (e) => {
         l.addEventListener("mouseenter", (event1) => {
             event1.target.querySelector(".cross").style.visibility = "visible"
             event.target.querySelector(".cross").addEventListener("click", (event3) => {
+                
                 let node = event3.target.parentNode;
+                console.log("delete id : ", node.getAttribute("id"))
+                let id = node.getAttribute("id")
+                delete data[id]
                 node.remove();
                 event.stopImmediatePropagation();
+                domRefresh()
             })
         })
         l.addEventListener("mouseleave", (event2) => {
@@ -218,4 +236,50 @@ function drop_handler(ev) {
     
 }
 
+// -------------------------------------------------------- DOM REFRESH ----------------------------------------------------------
+function domRefresh(){
+    let ul = document.querySelector("ul")
+    ul.innerHTML = "";
+    let keys = Object.keys(data)
+    for (let i = 0; i < keys.length; i++) {
+        console.log(keys[i])
+        // ul.appendChild(data[keys[i]])
+        const li = document.createElement("li")
+        li.setAttribute("id", keys[i])
+        li.setAttribute("draggable", "true")
+        li.setAttribute("ondragstart", "dragstart_handler(event)")
+        li.setAttribute("ondrop", "drop_handler(event)")
+        li.setAttribute("ondragover", "dragover_handler(event)")
+        
+        // li.className = "list"
+        const input = document.createElement("input")
+        input.setAttribute('type', 'checkbox')
+        input.checked = data[keys[i]].completed
+        input.value = data[keys[i]].completed
+        input.setAttribute('id', 'toggle')
+
+        // li.appendChild(input)
+
+        const label = document.createElement("label")
+        label.textContent = data[keys[i]].text
+       
+        let div = document.createElement("div")
+        div.appendChild(input)
+        div.appendChild(label)
+        li.appendChild(div)
+        let cross = document.createElement("div")
+
+        cross.textContent = "\u274C"
+        cross.style.fontSize = "8px"
+        cross.className = "cross"
+        li.append(cross)
+        li.style.display = "flex"
+        li.style.alignItems = "center"
+        li.style.justifyContent = "space-between"
+        li.querySelector(".cross").style.visibility = "hidden"
+        
+        document.querySelector("ul").appendChild(li)
+
+    }
+}
 
